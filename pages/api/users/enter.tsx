e689e3a87269bec6a9da5b6b765bdd4ca0a2250a
audgks5551 <audgks5551@daum.net>
2022-05-21 18:48:29 +0900

    twilio 핸드폰 인증문자 확인 및 구현

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,18 +1,20 @@
 import {NextApiRequest, NextApiResponse} from "next";
-import withHandler from "@libs/server/withHandler";
+import withHandler, {ResponseType} from "@libs/server/withHandler";
 import client from "@libs/server/client";
-
+import twilio from "twilio";
+const twilioClient = twilio(process.env.TWILIO_ID, process.env.TWILIO_TOKEN)
 async function handler(
     req: NextApiRequest,
-    res: NextApiResponse
+    res: NextApiResponse<ResponseType>
 ) {
-    console.log(req.body)
     const { email, phone } = req.body
-    const user = phone ? {phone: +phone} : {email}
+    const user = phone ? {phone: +phone} : email ? {email} : null;
+    if (!user) return res.status(400).end();
+    const payload = Math.floor(100000 + Math.random() * 900000) + "";
 
     const token = await client.token.create({
         data: {
-            payload: "1234",
+            payload,
             user: {
                 connectOrCreate: {
                     where: {
@@ -26,10 +28,20 @@ async function handler(
             }
         }
     });
+    if (phone) {
+        const message = await twilioClient.messages.create({
+            messagingServiceSid: process.env.TWILIO_MESSAGE_ID,
+            to: process.env.PHONE_NUMBER!,
+            body: `확인 번호 [${payload}]`
+        })
+        console.log(message)
+    }
 
     console.log(token)
 
-    return res.status(200).end();
+    return res.json({
+        ok: true,
+    });
 }
 
 export default withHandler("POST", handler);
