d220e6bfe8abd91157eb3c982df01c6ad63726bb
audgks5551 <audgks5551@daum.net>
2022-05-21 14:50:40 +0900

    fetch를 이용하여 api 통신

--- components/input.tsx
+++ components/input.tsx
@@ -1,11 +1,25 @@
+import type { UseFormRegisterReturn } from "react-hook-form/dist/types/form";
+
 interface InputProps {
     label: string;
     name: string;
     kind?: "text" | "phone" | "price";
-    [key: string]: any;
+    register: UseFormRegisterReturn
+    type: string
+    required: boolean
 }
 
-export default function Input({label, name, kind = "text", ...rest}: InputProps) {
+export default function Input(
+
+    {
+        label,
+        name,
+        kind = "text",
+        register,
+        type,
+        required
+    }: InputProps) {
+
     return (
         <div>
             <label
@@ -18,7 +32,9 @@ export default function Input({label, name, kind = "text", ...rest}: InputProps)
                 <div className="rounded-md relative flex  items-center shadow-sm">
                     <input
                         id={name}
-                        {...rest}
+                        type={type}
+                        required={required}
+                        {...register}
                         className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                     />
                 </div>
@@ -30,7 +46,9 @@ export default function Input({label, name, kind = "text", ...rest}: InputProps)
                     </div>
                     <input
                         id={name}
-                        {...rest}
+                        type={type}
+                        required={required}
+                        {...register}
                         className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                     />
                     <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
@@ -45,7 +63,9 @@ export default function Input({label, name, kind = "text", ...rest}: InputProps)
           </span>
                     <input
                         id={name}
-                        {...rest}
+                        type={type}
+                        required={required}
+                        {...register}
                         className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                     />
                 </div>
--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,15 +1,53 @@
 import {useForm} from "react-hook-form";
 
 export default function Forms() {
-    const { register, watch } = useForm();
-    console.log(register("hi"));
-    console.log(watch());
+    const { register, handleSubmit } = useForm();
+    const onValid = () => {
+        console.log("valid")
+    }
+    const onInValid = () => {
+        console.log("InValid")
+    }
+
     return (
         <>
-            <form>
-                <input {...register("username")} type="text" placeholder="Username" required />
-                <input {...register("email")} type="email" placeholder="Email" required />
-                <input {...register("password")} type="password" placeholder="Password" required />
+            <form onSubmit={handleSubmit(onValid, onInValid)}>
+                <input {...register(
+                    "username",
+                    {
+                        required: "이름을 적어주세요",
+                        minLength: {
+                            value: 1,
+                            message: "이름은 최소 1글자 이상 적어야합니다."
+                        }
+                    })}
+                       type="text"
+                       placeholder="Username"
+                />
+                <input {...register(
+                    "email",
+                    {
+                        required: "이메일을 적어주세요",
+                        minLength: {
+                            value: 1,
+                            message: "이메일은 최소 1글자 이상 적어야합니다."
+                        }
+                    })}
+                       type="email"
+                       placeholder="Email"
+                />
+                <input {...register(
+                    "password",
+                    {
+                        required: "비밀번호를 적어주세요",
+                        minLength: {
+                            value: 8,
+                            message: "비밀번호는 최소 8글자 이상 적어야합니다."
+                        }
+                    })}
+                       type="password"
+                       placeholder="Password"
+                />
                 <input type="submit" value="계정 생성"/>
             </form>
         </>
